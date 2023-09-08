4b728283915e4f994f6c6f260ff0591f57b08e97
MUHAMMAD-TALHA-ATHAR <112604191+MUHAMMAD-TALHA-ATHAR@users.noreply.github.com>
2023-09-08 23:16:59 +0500

    Add files via upload

---
+++ assignment5.js
@@ -0,0 +1,55 @@
+// Todo Assignment 5
+
+//! Question # 1
+// var a = +prompt("Enter first number:");
+// var b = +prompt("Enter second number");
+// c = a + b
+// document.write("Sum of "+a+" and "+b+" is "+c);
+
+//! Question # 2
+// var a = +prompt("Enter first number:");
+// var b = +prompt("Enter second number");
+// c = a - b
+// document.write("Subtraction of "+a+" and "+b+" is "+c);
+
+// var a = +prompt("Enter first number:");
+// var b = +prompt("Enter second number");
+// c = a * b
+// document.write("Multiplication of "+a+" and "+b+" is "+c);
+
+// var a = +prompt("Enter first number:");
+// var b = +prompt("Enter second number");
+// c = a / b
+// document.write("Division of "+a+" and "+b+" is "+c);
+
+// var a = +prompt("Enter first number:");
+// var b = +prompt("Enter second number");
+// c = a % b
+// document.write("Modulus of "+a+" and "+b+" is "+c);
+
+//! Question # 3
+// var myVariable;
+// document.write("Value after value decleration is : " + myVariable );
+// myVariable = 5;
+// document.write("initial value is :"+ myVariable);
+// myVariable++;
+// document.write("Value after increment is :"+ myVariable);
+// myVariable = myVariable + 7;
+// document.write("Value after addition is :" + myVariable);
+// myVariable--;
+// document.write("Value after decrement is :" + myVariable);
+// myVariable = myVariable%3;
+// document.write("The Remainder is : "+myVariable)
+
+//! Question # 4
+// var oneMovieTicket = 600;
+// var costOf5MovieTickets = oneMovieTicket * 5;
+// document.write("Total cost to buy 5 movie tickets is "+costOf5MovieTickets +" PKR")
+
+//! Question # 6
+// var celsius = 25;
+// var fahrenheit = 70;
+// var C = (fahrenheit-32)*5/9;
+// var F = (celsius*5/9)+32;
+// document.write(celsius+" C is "+F+" F");
+// document.write(fahrenheit+" F "+ C + " C ");
